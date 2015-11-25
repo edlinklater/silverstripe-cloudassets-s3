eba8a63382c45f1747e38b8e2737179b73ae5270
Ed Linklater <ed@silverstripe.com>
2015-11-25 22:51:22 +1300

    Add rudimentary unit test for connectivity to S3 (fixes #5)

---
+++ tests/CloudAssetsS3Test.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Requires a mapping in your config that includes assets/Uploads
+ */
+
+class CloudAssetsS3Test extends SapphireTest {
+
+	private $bucket;
+
+	function setUp() {
+		$this->bucket = CloudAssets::inst()->map('assets/Uploads/CloudAssetsS3Test.txt');
+		parent::setUpOnce();
+	}
+
+	function testInstantiateBucket() {
+		$this->assertInstanceOf('S3Bucket', $this->bucket);
+	}
+
+	function testGetFile() {
+		if(!$this->bucket) $this->fail('Cannot continue without valid bucket');
+
+		$file = new File();
+		$file->setName('S3ConnectionTest.txt');
+		try {
+			$size = $this->bucket->getFileSize($file);
+		} catch(Exception $e) {
+			$error = true;
+			echo $e->getMessage();
+		}
+
+		$this->assertFalse(isset($error));
+	}
+
+}
